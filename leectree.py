ff0d7fd523c7e98f241fd0cfe6cf36c0c0e5a83c
Zhanpeng Liang <hectorilles@gmail.com>
2017-01-04 12:47:50 -0800

    modified

--- leectree.py
+++ leectree.py
@@ -1,32 +1,39 @@
 # Leetcode Tree generation
-import random, sys
+import random, sys, argparse
 
-max_val = 100
-null_weight = 0.1
-n = 20
+# argc = len(sys.argv)
+# argv = sys.argv
 
-argc = len(sys.argv)
-argv = sys.argv
+# if argc > 4:
+#     print('Usage: leectree [ n [null_weight [max_val]] ]')
+#     quit()
+# 
+# if argc > 1:
+#     n = int(argv[1])
+# if argc > 2:
+#     null_weight = float(argv[2])
+#     if null_weight < 0 or null_weight >= 1:
+#         print('second argument should be between 0 and 1')
+#         quit()
+# if argc == 4:
+#     max_val = int(argv[3])
+parser = argparse.ArgumentParser(description='Randomly generate a list number')
+parser.add_argument('-n', metavar = 'N', type=int, default=10)
+parser.add_argument('-max', metavar = 'Max', type=int, default=100)
+parser.add_argument('-w', metavar = 'Null_Weight', type=float, default=0.2)
+parser.add_argument('-sym', metavar = 'Null_Symbol', type=str, default='null')
+args = parser.parse_args()
 
-if argc > 4:
-    print('Usage: leectree [ n [null_weight [max_val]] ]')
-    quit()
-
-if argc > 1:
-    n = int(argv[1])
-if argc > 2:
-    null_weight = float(argv[2])
-    if null_weight < 0 or null_weight >= 1:
-        print('second argument should be between 0 and 1')
-        quit()
-if argc == 4:
-    max_val = int(argv[3])
+n = args.n
+max_val = args.max
+null_weight = args.w
+null_symbol = args.sym
 
     
 n_null = int(null_weight * n)
 n_num = n - n_null
 l = random.sample(range(max_val), n_num)
-l[n_num:n_num] = ['null'] * n_null
+l[n_num:n_num] = [null_symbol] * n_null
 random.shuffle(l)
 
 print('[', end='')
--- randNumList.py
+++ randNumList.py
@@ -3,6 +3,10 @@ import random, argparse
 parser = argparse.ArgumentParser(description='Randomly generate a list number')
 parser.add_argument('-n', metavar = 'N', type=int, default=10)
 parser.add_argument('-max', metavar = 'Max', type=int, default=20)
+parser.add_argument('-s', default=False, action='store_true')
 args = parser.parse_args()
 
-print ( [random.randrange(args.max) for _ in range(args.n)] )
+list = [random.randrange(args.max) for _ in range(args.n)]
+if args.s:
+    list.sort()
+print ( list )
